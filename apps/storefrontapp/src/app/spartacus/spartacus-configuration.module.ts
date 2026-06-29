81223a450db48128c18dcd43f7397be2d53b232d
Norbert Papp <norbert.papp02@sap.com>
2026-06-29 11:03:56 +0200

    feat: align SPA with ccv2 deployment requirements

--- apps/storefrontapp/src/app/spartacus/spartacus-configuration.module.ts
+++ apps/storefrontapp/src/app/spartacus/spartacus-configuration.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { translationChunksConfig, translationsEn } from "@spartacus/assets";
-import { FeaturesConfig, I18nConfig, OccConfig, provideConfig, provideConfigFactory, SiteContextConfig } from "@spartacus/core";
+import { FeaturesConfig, I18nConfig, provideConfig, provideConfigFactory, SiteContextConfig } from "@spartacus/core";
 import { defaultCmsContentProviders, layoutConfigFactory, mediaConfig } from "@spartacus/storefront";
 
 @NgModule({
   declarations: [],
   imports: [
   ],
-  providers: [provideConfigFactory(layoutConfigFactory), provideConfig(mediaConfig), ...defaultCmsContentProviders, provideConfig(<OccConfig>{
-    backend: {
-      occ: {
-        baseUrl: 'https://api.cc3ihxtp03-mcpacppoc1-p3-public.model-t.myhybris.cloud',
-      }
-    },
-  }), provideConfig(<SiteContextConfig>{
+  providers: [provideConfigFactory(layoutConfigFactory), provideConfig(mediaConfig), ...defaultCmsContentProviders, provideConfig(<SiteContextConfig>{
     context: {},
   }), provideConfig(<I18nConfig>{
     i18n: {
